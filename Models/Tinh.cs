57af4af551b0d79111597f47585ecff7f40774ba
PhamNhanHau <phamnhanhau23042005@gmail.com>
2026-05-11 22:39:51 +0700

    Hoan thanh model tinh

--- Models/Tinh.cs
+++ Models/Tinh.cs
@@ -3,8 +3,45 @@ namespace DoAnCSharp.Models
 {
     public class Tinh
     {
-        public int maTinh { get; set; }
-        public string tenTinh { get; set; }
+        private int maTinh;
+        public int MaTinh  
+        {
+            get
+            {
+                return maTinh;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    maTinh = value;
+                }
+            }
+        }
+        private string tenTinh;
+        public string TenTinh
+        {
+            get
+            {
+                return tenTinh;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    tenTinh = value;
+                }
+            }
+        }
+        public Tinh()
+        {
+        }
+        public Tinh(int maTinh, string tenTinh)
+        {
+            this.maTinh = maTinh;
+            this.tenTinh = tenTinh;
+        }
+        
 
     }
 
