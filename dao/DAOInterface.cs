7ec461c9245f16e4014213a7fa4257dd26b2780f
PhamNhanHau <phamnhanhau23042005@gmail.com>
2026-05-28 10:41:33 +0700

    sua loi nho uml, them 2 package database va Data access object de hoan thanh yeu cau cua thay khong biet co thuc su huu dung khong nua

---
+++ dao/DAOInterface.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoAnCSharp.model
+{
+    public interface DAOInterface<T>
+    {
+        public int insert(T t);
+        public int update(T t);
+        public int delete(T t);
+        public List<T> selectAll(T t);
+        public T selectByID(int ID);
+        public List<T> selectByCondition(String condition);
+
+    }
+}
